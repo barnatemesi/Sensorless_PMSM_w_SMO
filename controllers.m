c6595bfcd7ca1a29d3ea7e3c0586fa94c0f71fe0
barnatemesi <98287245+barnatemesi@users.noreply.github.com>
2022-02-06 15:52:05 +0100

    Add files via upload
    5th controller was added with decreased bandwidth

--- controllers.m
+++ controllers.m
@@ -1,53 +1,65 @@
 %Controllers
 
-switch 4
-case 1
-    
-    Ki_speed          = 2;  
-    Kp_speed          = 0.06;
-    Ka_speed          = 0;
-    
-    Ki_id          = 400;
-    Kp_id          = 4;
-
-    Ki_iq          = 400;     
-    Kp_iq          = 4;  
-
-case 2
-    % 
-    Ki_speed          = 3;  
-    Kp_speed          = 0.4;
-    Ka_speed          = 2;
-    
-    Ki_id          = 463;
-    Kp_id          = 3.8;
-
-    Ki_iq          = 463;     
-    Kp_iq          = 3.8;    
-    
-case 3 
-% 
-    Ki_speed          = 3;  
-    Kp_speed          = 0.3;
-    Ka_speed          = 3.5;
-    
-    Ki_id          = 463;
-    Kp_id          = 3.8;
-
-    Ki_iq          = 463;     
-    Kp_iq          = 3.8;
-    
-case 4
-    %Lab values 
-    Ki_speed          = 3;  
-    Kp_speed          = 0.4;
-    Ka_speed          = 3;
-    
-    Ki_id          = 463;
-    Kp_id          = 3.8;
-
-    Ki_iq          = 463;     
-    Kp_iq          = 3.8;
+switch 5
+    case 1
+        
+        Ki_speed          = 2;
+        Kp_speed          = 0.06;
+        Ka_speed          = 0;
+        
+        Ki_id          = 400;
+        Kp_id          = 4;
+        
+        Ki_iq          = 400;
+        Kp_iq          = 4;
+        
+    case 2
+        %
+        Ki_speed          = 3;
+        Kp_speed          = 0.4;
+        Ka_speed          = 2;
+        
+        Ki_id          = 463;
+        Kp_id          = 3.8;
+        
+        Ki_iq          = 463;
+        Kp_iq          = 3.8;
+        
+    case 3
+        %
+        Ki_speed          = 3;
+        Kp_speed          = 0.3;
+        Ka_speed          = 3.5;
+        
+        Ki_id          = 463;
+        Kp_id          = 3.8;
+        
+        Ki_iq          = 463;
+        Kp_iq          = 3.8;
+        
+    case 4
+        %Lab values
+        Ki_speed          = 3;
+        Kp_speed          = 0.4;
+        Ka_speed          = 3;
+        
+        Ki_id          = 463;
+        Kp_id          = 3.8;
+        
+        Ki_iq          = 463;
+        Kp_iq          = 3.8;
+        
+    case 5
+        %Lab values
+        Ki_speed          = 1;
+        Kp_speed          = 0.1;
+        Ka_speed          = 5;
+        
+        Ki_id          = 463;
+        Kp_id          = 3.8;
+        
+        Ki_iq          = 463;
+        Kp_iq          = 3.8;
 end
 
 model_parameters
@@ -56,10 +68,10 @@ model_parameters
 % tau_mech=J/B_m;
 % G_mech=(1/(s*tau_mech+1))*(Ki_speed+Kp_speed*s)/s;
 % [num,den]=tfdata(G_mech);
-% 
+%
 % zeta=1;
 % w_n=500*2*pi;%(1/tau_mech*zeta)*1000;
 % Ki_est=w_n^2;
 % Kp_est=2*zeta*w_n;
-% 
+%
 % G_est=(Kp_est*s+Ki_est)/(s^2+Kp_est*s+Ki_est);
